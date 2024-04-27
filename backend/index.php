fcf1b2b4641eb35e4755a322031a93be44320cae
emirkugic <emir.kugic@hotmail.com>
2024-04-27 20:28:21 +0200

    user based authorization (admin and user)

--- backend/index.php
+++ backend/index.php
@@ -3,16 +3,19 @@
 require 'vendor/autoload.php';
 require 'config.php';
 
-
 Flight::before('start', function (&$params, &$output) {
     $headers = getallheaders();
     $path = Flight::request()->url;
 
-    $excluded_paths = ['/auth/login', '/auth/register'];
+    $excluded_paths = [
+        '/auth/login',
+        '/auth/register'
+    ];
 
     if (!in_array($path, $excluded_paths)) {
         $jwt = $headers['Authorization'] ?? '';
         $jwt = str_replace('Bearer ', '', $jwt);
+
         if ($jwt) {
             try {
                 $decoded = \Firebase\JWT\JWT::decode($jwt, new \Firebase\JWT\Key(JWT_SECRET_KEY, 'HS256'));
@@ -28,8 +31,8 @@ Flight::before('start', function (&$params, &$output) {
     }
 });
 
-
 require 'rest/routes/AuthRoutes.php';
 require 'rest/routes/UserRoutes.php';
 require 'rest/routes/ProductRoutes.php';
+
 Flight::start();
--- backend/rest/routes/ProductRoutes.php
+++ backend/rest/routes/ProductRoutes.php
@@ -1,40 +1,34 @@
 <?php
 
 require_once __DIR__ . '/../services/ProductService.class.php';
+require_once __DIR__ . '/../../middleware.php';
 
 Flight::group('/products', function () {
-
     Flight::route('GET /', function () {
         $order = Flight::request()->query['order'] ?? '-id';
         $product_service = new ProductService();
         $products = $product_service->get_all_products($order);
-        Flight::json($products, 201);
+        Flight::json($products);
     });
 
     Flight::route('GET /popular', function () {
         $product_service = new ProductService();
         $products = $product_service->get_products_by_popularity();
-        if ($products) {
-            Flight::json($products, 201);
-        } else {
-            Flight::halt(404, 'No popular products found');
-        }
+        Flight::json($products);
     });
-
 
     Flight::route('GET /@id', function ($id) {
         $product_service = new ProductService();
         $product = $product_service->get_product_by_id($id);
         if ($product) {
-            Flight::json($product, 201);
+            Flight::json($product);
         } else {
             Flight::halt(404, 'Product not found');
         }
     });
 
-
-
     Flight::route('POST /add', function () {
+        authorize("ADMIN");
         $data = Flight::request()->data->getData();
         $product_service = new ProductService();
         $product = $product_service->add_product($data);
@@ -42,17 +36,19 @@ Flight::group('/products', function () {
     });
 
     Flight::route('PUT /update/@id', function ($id) {
+        authorize("ADMIN");
         $data = Flight::request()->data->getData();
         $product_service = new ProductService();
         $updated_product = $product_service->update_product($id, $data);
         if ($updated_product) {
-            Flight::json(201);
+            Flight::json($updated_product, 201);
         } else {
             Flight::halt(400, 'Failed to update product');
         }
     });
 
     Flight::route('DELETE /delete/@id', function ($id) {
+        authorize("ADMIN");
         $product_service = new ProductService();
         $success = $product_service->delete_product_by_id($id);
         if ($success) {
@@ -61,15 +57,4 @@ Flight::group('/products', function () {
             Flight::halt(404, 'Product not found or could not be deleted');
         }
     });
-
-    // TODO - doesn't work, is it needed?
-    // Flight::route('POST /increment-bought/@id', function ($id) {
-    //     $product_service = new ProductService();
-    //     $success = $product_service->increment_product_bought($id);
-    //     if ($success) {
-    //         Flight::json(['message' => "Product bought count incremented"]);
-    //     } else {
-    //         Flight::halt(400, 'Failed to increment product bought count');
-    //     }
-    // });
 });
