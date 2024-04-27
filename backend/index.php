cb2090aeef45ab4eabec4773d53bb8d97bb237dc
emirkugic <emir.kugic@hotmail.com>
2024-04-27 17:48:58 +0200

    middleware added

--- backend/index.php
+++ backend/index.php
@@ -1,9 +1,35 @@
 <?php
 
 require 'vendor/autoload.php';
+require 'config.php';
+
+
+Flight::before('start', function (&$params, &$output) {
+    $headers = getallheaders();
+    $path = Flight::request()->url;
+
+    $excluded_paths = ['/auth/login', '/auth/register'];
+
+    if (!in_array($path, $excluded_paths)) {
+        $jwt = $headers['Authorization'] ?? '';
+        $jwt = str_replace('Bearer ', '', $jwt);
+        if ($jwt) {
+            try {
+                $decoded = \Firebase\JWT\JWT::decode($jwt, new \Firebase\JWT\Key(JWT_SECRET_KEY, 'HS256'));
+                Flight::set('user', (array) $decoded);
+            } catch (\Exception $e) {
+                Flight::halt(401, 'Unauthorized: ' . $e->getMessage());
+                return;
+            }
+        } else {
+            Flight::halt(401, 'Unauthorized: Token not provided');
+            return;
+        }
+    }
+});
+
+
 require 'rest/routes/AuthRoutes.php';
 require 'rest/routes/UserRoutes.php';
 require 'rest/routes/ProductRoutes.php';
-
-
 Flight::start();
